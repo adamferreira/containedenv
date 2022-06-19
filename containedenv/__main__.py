28f617b675b42e02105b85d5c90a77fa3e38ff1d
adamferreira <adam.ferreira.dc@gmail.com>
2022-06-19 18:11:59 +0200

    containedenv project setup

---
+++ containedenv/__main__.py
@@ -0,0 +1,15 @@
+import argparse
+
+
+def get_argparser():
+    parser = argparse.ArgumentParser(
+        description="Build a contained dev environment"
+    )
+
+    return parser
+
+def main():
+    return None
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="containedenv",
+    version="0.0.1",
+    author="Adam Ferreira",
+    author_email="adam.ferreira.dc@gmail.com",
+    description="Dev containers creation tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/adamferreira/containedenv",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={
+        "containedenv": "containedenv"
+    },
+    packages = ["containedenv"], 
+    python_requires=">=3.0",
+    install_requires=[
+       "pyyaml",
+       "docker"
+   ],
+    entry_points= {
+            "console_scripts": [
+                "containedenv = containedenv.__main__:main"
+        ]
+    }
+)
