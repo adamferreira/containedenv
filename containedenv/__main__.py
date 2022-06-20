9613722a513c3ff89bc15e2eec892f797c5cd1f6
adamferreira <adam.ferreira.dc@gmail.com>
2022-06-20 18:02:10 +0200

    --no-build arg

--- containedenv/__main__.py
+++ containedenv/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+import docker
 
 
 def get_argparser():
@@ -6,10 +7,19 @@ def get_argparser():
         description="Build a contained dev environment"
     )
 
-    return parser
+    parser.add_argument(
+        "--no-build",
+        dest="build",
+        action="store_false",
+        help=(
+            "Do not actually build the image. Useful in conjunction " "with --debug."
+        ),
+    )
 
 def main():
     return None
 
 if __name__ == "__main__":
     main()
+    client = docker.from_env()
+    print(client.containers)
