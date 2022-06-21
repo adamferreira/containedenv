13e889a3ad9c5afc5b20d4ac4a88d4d027ffe910
adamferreira <adam.ferreira.dc@gmail.com>
2022-06-21 15:45:01 +0200

    RUN and FROM statements

--- containedenv/dockerfile.py
+++ containedenv/dockerfile.py
@@ -1,8 +1,24 @@
+from typing import List, Union
 import docker
 
 class DockerFile:
-    def __init__(self, path:str):
-        self._path = path
+    def __init__(self):
+        self._statements = []
+        self._from = "ubuntu:latest"
 
-    def FROM(self, image:str):
+    def dump(self, path:str) -> None:
+        with open(path, "w+") as f:
+            f.write("FROM" + " " + self._from + "\n\n")
+            f.writelines(self._statements)
+
+    def FROM(self, image:str) -> "DockerFile":
+        self._from = image
+        return self
+
+    def RUN(self, statements:Union[str, List[str]]) -> "DockerFile":
+        if isinstance(statements, str):
+            return self.RUN([statements])
+
+        self._statements.extend([f"{self.RUN.__name__} {s} \n" for s in statements])
+        self._statements.append("")
         return self
