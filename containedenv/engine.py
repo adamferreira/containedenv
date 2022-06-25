3190487bb7d1975e555eedb263ec47d0ef1f9026
adamferreira <adam.ferreira.dc@gmail.com>
2022-06-25 16:07:20 +0200

    First Running container

--- containedenv/engine.py
+++ containedenv/engine.py
@@ -11,11 +11,11 @@ class ContainedEnv:
         self._dockerclient = docker.from_env()
 
     def build_image(self):
-        file = os.path.join(self._workspace, "Dockerfile")
-        self._dockerfile.dump(
-            file
-        )
-        self._dockerclient.images.build(
+        file = os.path.join(self._workspace, "Dockerfile.test")
+        #self._dockerfile.dump(
+        #    file
+        #)
+        self._image, _ = self._dockerclient.images.build(
             path = self._workspace,
             dockerfile = file,
             tag = "containedenv:0.0.1",
@@ -26,4 +26,17 @@ class ContainedEnv:
             # Always remove intermediate containers, even after unsuccessful builds
             forcerm = True
         )
+        self._container = self._dockerclient.containers.run(
+            image = "containedenv:0.0.1",
+            #image = self._image.id,
+            command = "bash",
+            name = "MyContainer",
+            hostname = "containedenv",
+            tty = True,
+            detach = True
+        )
+        self._container.logs()
+        _, out = self._container.exec_run("ls")
+        print(out.decode('utf-8'))
+        
         return self._image
